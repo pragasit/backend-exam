b7556e671d1bb4be20f2d0f44f433ff984b87dd6
pragasit <32674358+pragasit@users.noreply.github.com>
2024-05-01 07:24:07 +0700

    4.0 test

--- 4_number_to_roman/main.py
+++ 4_number_to_roman/main.py
@@ -19,5 +19,37 @@ output = number can not less than 0
 
 class Solution:
 
+    def __init__(self):
+        self.numbers = {
+            1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V',
+            6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX'
+        }
+
+        self.places = {1: '', 10: 'X'}
+        self.places2 = {2: 'XX'}
     def number_to_roman(self, number: int) -> str:
-        pass
+        if number < 0:
+            return "number can not less than 0"
+        num_digit = list(str(number))
+        num_digit = list(map(int, num_digit))
+        number_roman = []
+        for place in [10, 1]:
+            digit = number // place
+            number %= place
+            if digit > 0:
+                if place == 10 and digit == 1:
+                    number_roman.append(self.places[10])
+                else:
+                    number_roman.append(self.numbers[digit])
+        str1 = ""
+        for i in number_roman:
+            str1 += i
+        return str1
+
+
+
+def main():
+    sol = Solution()
+    num = int(input())
+    print(sol.number_to_roman(num))
+main()
