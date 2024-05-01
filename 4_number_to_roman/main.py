29c7f033b146fda1882b9f548c78aa999ea61ca6
pragasit <32674358+pragasit@users.noreply.github.com>
2024-05-01 07:44:30 +0700

    4.1

--- 4_number_to_roman/main.py
+++ 4_number_to_roman/main.py
@@ -22,29 +22,31 @@ class Solution:
     def __init__(self):
         self.numbers = {
             1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V',
-            6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX'
+            6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX', 10: 'X', 
+            40: 'XL', 50: 'L', 90: 'XC', 100: 'C', 
+            400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'
         }
-
-        self.places = {1: '', 10: 'X'}
-        self.places2 = {2: 'XX'}
     def number_to_roman(self, number: int) -> str:
         if number < 0:
             return "number can not less than 0"
         num_digit = list(str(number))
         num_digit = list(map(int, num_digit))
-        number_roman = []
-        for place in [10, 1]:
-            digit = number // place
-            number %= place
-            if digit > 0:
-                if place == 10 and digit == 1:
-                    number_roman.append(self.places[10])
-                else:
-                    number_roman.append(self.numbers[digit])
-        str1 = ""
-        for i in number_roman:
-            str1 += i
-        return str1
+        number_roman = ""
+        for i in num_digit:
+            number_roman += self.numbers[i]
+        return number_roman
+        # for place in [10, 1]:
+        #     digit = number // place
+        #     number %= place
+        #     if digit > 0:
+        #         if place == 10 and digit == 1:
+        #             number_roman.append(self.places[10])
+        #         else:
+        #             number_roman.append(self.numbers[digit])
+        # str1 = ""
+        # for i in number_roman:
+        #     str1 += i
+        # return str1
 
 
 
